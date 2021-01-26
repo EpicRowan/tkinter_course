de7ff0a52453cbd98a1849fa97142b525b27a0fa
Rowan <shepherd.rowan.k@gmail.com>
2021-01-26 14:20:50 -0800

    Replacing pack with grid system

--- gui.py
+++ gui.py
@@ -3,7 +3,12 @@ from tkinter import *
 root = Tk()
 
 firstLabel = Label(root, text="Hello World!")
+secondLabel = Label(root, text="I like puppies!")
 
-firstLabel.pack()
+# firstLabel.pack()
+
+firstLabel.grid()
+secondLabel.grid()
+
 
 root.mainloop()
