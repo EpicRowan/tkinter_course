c601eeb551f4c9a7dc84a6e0d62d287974d61003
Rowan <shepherd.rowan.k@gmail.com>
2021-01-27 14:46:37 -0800

    Added file with buttons

---
+++ buttons.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+
+firstbutton = Button(root, text = "Click me!")
+
+firstbutton.pack()
+
+root.mainloop()
--- gui.py
+++ gui.py
@@ -2,11 +2,14 @@ from tkinter import *
 
 root = Tk()
 
+#Create the things
+
 firstLabel = Label(root, text="Hello World!")
 secondLabel = Label(root, text="I like puppies!")
 
-# firstLabel.pack()
+#Put them on the screen
 
+# firstLabel.pack()
 firstLabel.grid(row=0, column=0)
 secondLabel.grid(row=1, column=0)
 
