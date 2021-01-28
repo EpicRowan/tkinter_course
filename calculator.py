8378a30a0001002400c7402b9635c8839f5f174d
Rowan <shepherd.rowan.k@gmail.com>
2021-01-28 14:28:20 -0800

    Added clear, +, and = buttons with columnspan to allow them to fit

--- calculator.py
+++ calculator.py
@@ -9,6 +9,8 @@ e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 def button_add():
 	return
+
+
 button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
 button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
 button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
@@ -19,6 +21,9 @@ button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
 button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
 button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
 button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+button_add = Button(root, text="+", padx=39, pady=20, command=button_add)
+button_equal = Button(root, text="=", padx=91, pady=20, command=button_add)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_add)
 
 # Put buttons on the screen
 
@@ -32,6 +37,8 @@ button_7.grid(row=1, column=0)
 button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 button_0.grid(row=4, column=0)
-
+button_add.grid(row=5, column=0)
+button_equal.grid(row=4, column=1, columnspan=2)
+button_clear.grid(row=5, column=1, columnspan=2)
 
 root.mainloop()
