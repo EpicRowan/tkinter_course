b5bb573124b43480d911920592c3ddea5a0bbec6
Rowan <shepherd.rowan.k@gmail.com>
2021-02-01 17:30:01 -0800

    Successfully packed image onto label

--- images.py
+++ images.py
@@ -1,7 +1,11 @@
 from tkinter import *
+from PIL import ImageTk,Image
 
 root = Tk()
 root.title("Insert images")
 
+my_img = ImageTk.PhotoImage(Image.open("shrimpart"))
+my_label = Label(image=my_img)
+my_label.pack()
 
 root.mainloop()
