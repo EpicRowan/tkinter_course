486cd018f3886580f088e1887fe923339788fe07
Rowan <shepherd.rowan.k@gmail.com>
2021-02-01 18:33:11 -0800

    Added button to close program

--- images.py
+++ images.py
@@ -8,4 +8,7 @@ my_img = ImageTk.PhotoImage(Image.open("shrimpart"))
 my_label = Label(image=my_img)
 my_label.pack()
 
+button_quit = Button(root,text="Close", command=root.quit)
+button_quit.pack()
+
 root.mainloop()
