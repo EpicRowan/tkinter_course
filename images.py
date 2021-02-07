510ee4d9f43f89d13a73e4e8e293c57df3472f17
Rowan <shepherd.rowan.k@gmail.com>
2021-02-06 16:25:14 -0800

    Reverted back to single button

--- images.py
+++ images.py
@@ -13,10 +13,5 @@ my_label.pack()
 button_quit = Button(root,text="Close",bg="purple", command=root.quit)
 button_quit.pack()
 
-button_test1 = Button(root,text="Test1", bg="grey", command=root.quit)
-button_test1.pack()
-
-button_test2 = Button(root,text="Test2", bg="white", command=root.quit)
-button_test2.pack()
 
 root.mainloop()
