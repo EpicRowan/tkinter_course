5407fe5655e0d19480d5d4439ce3b06114a60a49
Rowan <shepherd.rowan.k@gmail.com>
2021-02-08 14:20:51 -0800

    Setting up file for displaying messages

--- radio.py
+++ radio.py
@@ -11,5 +11,7 @@ Radiobutton(root, text="Option 1", variable=r, value=1).pack()
 Radiobutton(root, text="Option 2", variable=r, value=2).pack()
 Radiobutton(root, text="Option 3", variable=r, value=3).pack()
 
+myLabel = myLabel
+
 
 root.mainloop()
