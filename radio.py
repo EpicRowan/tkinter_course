7e5019befaa87e53ba2e37654d5779aba1aafb97
Rowan <shepherd.rowan.k@gmail.com>
2021-02-08 11:55:10 -0800

    Radio buttons successfully appear on GUI

--- radio.py
+++ radio.py
@@ -4,5 +4,12 @@ from PIL import ImageTk,Image
 root = Tk()
 root.title("Insert images")
 
+r = IntVar()
+
+
+Radiobutton(root, text="Option 1", variable=r, value=1).pack()
+Radiobutton(root, text="Option 2", variable=r, value=2).pack()
+Radiobutton(root, text="Option 3", variable=r, value=3).pack()
+
 
 root.mainloop()
