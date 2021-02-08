51667313a11aa57e3db11f0edf0e79e9d3bce871
Rowan <shepherd.rowan.k@gmail.com>
2021-02-07 18:45:05 -0800

    Creating a frame and packing the button into the frame

--- images.py
+++ images.py
@@ -5,12 +5,14 @@ root = Tk()
 root.title("Insert images")
 
 
-my_img = ImageTk.PhotoImage(Image.open("shrimpart"))
-my_label = Label(image=my_img)
-my_label.pack()
+# my_img = ImageTk.PhotoImage(Image.open("shrimpart"))
+# my_label = Label(image=my_img)
+# my_label.pack()
 
+frame = LabelFrame(root, text="A frame", padx=5, pady=5)
+frame.pack(padx=10, pady=10)
 
-button_quit = Button(root,text="Close",bg="purple", command=root.quit)
+button_quit = Button(frame,text="Close",bg="purple", command=root.quit)
 button_quit.pack()
 
 
