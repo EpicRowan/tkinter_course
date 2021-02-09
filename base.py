5643f5670ffed9d28f0877a153e6ce46c4f87de5
Rowan <shepherd.rowan.k@gmail.com>
2021-02-08 21:49:21 -0800

    Possible base for shrimp tracking future project

--- base.py
+++ base.py
@@ -3,5 +3,14 @@ from PIL import ImageTk,Image
 
 root = Tk()
 
+def open():
+	global my_img
+	top = Toplevel()
+	top.title("Enter Shrimp data")
+	my_img = ImageTk.PhotoImage(Image.open("shrimpart"))
+	my_label = Label(top, image=my_img).pack()
+
+
+btn = Button(root, text="Enter Shrimp data", command=open).pack()
 
 mainloop()
