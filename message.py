e1ac965107cbef11356e696fce6fb9280c1d56d4
Rowan <shepherd.rowan.k@gmail.com>
2021-02-08 21:43:56 -0800

    Setting up base.py for multiple windows

--- message.py
+++ message.py
@@ -1,9 +1,12 @@
 from tkinter import *
 from PIL import ImageTk,Image
+from tkinter import messagebox
 
 root = Tk()
 
+def popup():
+	messagebox.showinfo("This is the popup", "Hello world")
 
+Button(root, text="Popup", command=popup).pack()
 
-
-root.mainloop()
+mainloop()
